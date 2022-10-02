bc2fcc494b46b216268759bce800fc30533952ba
Vicentendistes <112288602+Vicentendistes@users.noreply.github.com>
2022-10-02 20:41:21 -0300

    Textura imantado prueba

--- scenes/imantado.gd
+++ scenes/imantado.gd
@@ -3,24 +3,18 @@ extends KinematicBody2D
 class_name Imantado
 
 onready var collision_shape_2d = $CollisionShape2D
-onready var icon = $Icon
-onready var icon_2 = $Icon2
 #export (int, 0, 50) onready var extents_x = 7
 #export (int, 0, 50) onready var extents_y = 7
 
 onready var shape_dimentions = collision_shape_2d.shape.get_extents()
 onready var width = shape_dimentions.x * self.scale.x
 onready var height = shape_dimentions.y * self.scale.y
-onready var ACCELERATION = 100 * self.scale.x * self.scale.y + 200
+onready var ACCELERATION = 300*self.scale.x * self.scale.y + 200
 
 var velocity = Vector2()
 var IMPULSE = 40
 var GRAVITY = 10
 
-func _ready():
-	#collision_shape_2d.shape.set_extents(Vector2(extents_x, extents_y))
-	icon.scale = shape_dimentions * 0.04
-	icon_2.scale = shape_dimentions * 0.032
 
 
 func _physics_process(delta):
@@ -35,7 +29,7 @@ func _physics_process(delta):
 			var difference = global_position - collision.collider.global_position
 			#print(difference)
 			if difference.y < -7:
-				print("choque")
+				#print("choque")
 				collision.collider.push(-difference.normalized() * IMPULSE, "x")
 				push(difference.normalized() * (IMPULSE+20), "x")
 				
