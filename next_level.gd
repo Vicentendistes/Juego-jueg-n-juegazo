b2d38b6b68e33b8555517d1a77d864decebbafd9
Vicentendistes <112288602+Vicentendistes@users.noreply.github.com>
2022-10-16 17:23:15 -0300

    Sistema de siguente nivel

--- next_level.gd
+++ next_level.gd
@@ -1,12 +1,34 @@
 extends Sprite
 
+onready var levels = []
+onready var current_level_index
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimationPlayer.play("ciclo")
 	var hitbox = $Area2D
-	hitbox.connect("body_exited", hitbox, "_on_hitbox_body_entered")
+	#checks levels folder and add path to levels list
+	var dir = Directory.new()
+	dir.open("res://scenes/levels/")
+	dir.list_dir_begin()
+	var i = 0
+	while true:
+		var file = dir.get_next()
+		if file == "":
+			break
+		elif not file.begins_with("."):
+			levels.append("res://scenes/levels/"+str(file))
+			#save current scene index
+			if "res://scenes/levels/"+str(file) == get_tree().current_scene.filename:
+				current_level_index = i
+			i += 1
+	dir.list_dir_end()
+	
+	#add signal to enter next level
+	hitbox.connect("body_entered", self, "_on_hitbox_body_entered", [current_level_index+1])
 	
 	
-func _on_hitbox_body_entered(body: Node):
+func _on_hitbox_body_entered(_body, i):
+	if i< len(levels) and get_tree().change_scene(levels[i]) != OK:
+		print("error al cambiar de escena")
 		
 	
